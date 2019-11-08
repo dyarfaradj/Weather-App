0a2ca182900ddb003601beb4e8afdfc2efe6c8bb
dyarfaradj <dyar.98@hotmail.com>
2019-11-08 23:49:41 +0100

    Added more info

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,7 +1,31 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { ListItem, Image } from "react-native-elements";
-
+function pad2(number) {
+  return (number < 10 ? "0" : "") + number;
+}
+function degToCompass(num) {
+  var val = Math.floor(num / 22.5 + 0.5);
+  var arr = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW"
+  ];
+  return arr[val % 16];
+}
 export default WeathCard = props => {
   return (
     <View style={styles.container}>
@@ -9,8 +33,20 @@ export default WeathCard = props => {
         source={require("../assets/images/PartCloudRainThunderDay.png")}
         style={{ width: 50, height: 50 }}
       />
-      <Text style={styles.title}>{props.data.validTime}</Text>
-      <Text style={styles.title}>{props.data.parameters[1].values[0]}</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{props.data.validTime}</Text>
+        <Text style={styles.celcius}>
+          {pad2(props.data.parameters[1].values[0])}
+        </Text>
+        <Text style={styles.wind}>
+          Wind: {degToCompass(props.data.parameters[3].values[0])}{" "}
+          {props.data.parameters[4].values[0]}m/s
+        </Text>
+        <Text style={styles.precipitation}>
+          Min: {props.data.parameters[12].values[0]} Max:{" "}
+          {props.data.parameters[13].values[0]}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -21,11 +57,25 @@ const styles = StyleSheet.create({
     height: 100,
     backgroundColor: "white",
     borderTopWidth: 1,
-    borderBottomWidth: 1,
-    borderTopColor: "black",
-    borderBottomColor: "black"
+    // borderBottomWidth: 1,
+    borderTopColor: "black"
+    // borderBottomColor: "black"
+  },
+  contentContainer: {
+    flexDirection: "column"
   },
   title: {
+    color: "black"
+  },
+  celcius: {
+    color: "black",
+    fontSize: 40,
+    fontWeight: "bold"
+  },
+  wind: {
+    color: "black"
+  },
+  precipitation: {
     color: "black"
   }
 });
